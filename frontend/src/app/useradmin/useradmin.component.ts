966be01d1fd8a5cdb1d12cfbc94c02cdf0d9a973
Thong Huynh <thongh@coutureconsulting.com>
2018-08-31 16:53:23 +0700

    update

--- frontend/src/app/useradmin/useradmin.component.ts
+++ frontend/src/app/useradmin/useradmin.component.ts
@@ -1,28 +1,35 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { MyService } from  './my.service';
-import { Observable } from 'rxjs/Observable';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-useradmin',
   templateUrl: './useradmin.component.html',
   styleUrls: ['./useradmin.component.css']
 })
-export class UseradminComponent implements OnInit  {
+    
+export class UseradminComponent {
 
-  characters: Observable<any[]>;
-  columns: string[];
-    
-  constructor(private atService: MyService) {}
-    
-  ngOnInit() {
-      this.columns = this.atService.getColumns(); 
-      //["name", "age", "species", "occupation"]
-      this.characters = this.atService.getCharacters();
-      //all data in mock-data.ts
-  }  
-    
-    
-  
-
+  displayedColumns = ['position', 'firstName', 'lastName', 'email'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+   
 }
+
+export interface Element {
+  position: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: 1, firstName: 'John', lastName: 'Doe', email: 'john@gmail.com'},
+  {position: 1, firstName: 'Mike', lastName: 'Hussey', email: 'mike@gmail.com'},
+  {position: 1, firstName: 'Ricky', lastName: 'Hans', email: 'ricky@gmail.com'},
+  {position: 1, firstName: 'Martin', lastName: 'Kos', email: 'martin@gmail.com'},
+  {position: 1, firstName: 'Tom', lastName: 'Paisa', email: 'tom@gmail.com'},
+  {position: 1, firstName: 'Tom', lastName: 'Paisa', email: 'tom@gmail.com'},
+  {position: 1, firstName: 'Tom', lastName: 'Paisa', email: 'tom@gmail.com'},
+  {position: 1, firstName: 'Tom', lastName: 'Paisa', email: 'tom@gmail.com'},
+  {position: 1, firstName: 'Tom', lastName: 'Paisa', email: 'tom@gmail.com'}
+];
